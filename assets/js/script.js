c48f437f591d23461beee7ded563c791905ea572
Travis Swift <travis.arthur.swift@gmail.com>
2023-04-14 23:03:09 -0500

    added working if else statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,18 @@ console.log('The function has been called, and the value of myVar is:', myVar);
 
 exampleFunction();
   
+
+var name = "John";
+let age = 30;
+const pi = 3.14159
+
+function checkEvenOdd() {
+    const numberInput = document.getElementById('numberInput');
+    const result = document.getElementById('result');
+    const number = parseInt(numberInput.value, 10);
+    if (number % 2 === 0) {
+        result.textContent = number + ' is even.';
+    } else {
+        result.textContent = number + ' is odd.';
+    }
+}
