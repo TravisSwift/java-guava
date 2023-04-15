d6f7acae1bd4de116d057bb629ce8be27906cd42
Travis Swift <travis.arthur.swift@gmail.com>
2023-04-14 21:27:57 -0500

    Created console log and simple function

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* Example of a console log  */
+console.log('Hello World!');
+
+/* A javascript function viewable in devtools */
+function exampleFunction() {
+var myVar = 42;
+console.log('The function has been called, and the value of myVar is:', myVar);
+}
+
+exampleFunction();
+  
